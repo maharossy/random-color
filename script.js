1d3d9f42d91a0587506dc5be2a717750e2ad3cad
Vladyslav Marynets <smarynec@gmail.com>
2022-11-08 20:39:22 +0200

    the colors stoped on click, and copiing color hex

--- script.js
+++ script.js
@@ -1,23 +1,12 @@
 const colons = document.querySelectorAll('.col');
 
 document.addEventListener('keydown', (e) => {
-  e.code === 'Space' ? document.location.reload() : '';
+  e.preventDefault();
+  //   e.code === 'Space' ? document.location.reload() : '';
+  if (e.code.toLowerCase() == 'space') {
+    getRandomColor();
+  }
 });
-// function createRandomColors() {
-//   const colorCode = '0123456789ABCDEF';
-
-//   color = '';
-//   for (let i = 0; i < 6; i++) {
-//     color += colorCode[Math.floor(Math.random() * colorCode.length)];
-//   }
-//   return '#' + color;
-// }
-function cracolor() {
-  const r = Math.random() * 255;
-  const g = Math.random() * 255;
-  const b = Math.random() * 255;
-  return rgb(r, g, b);
-}
 
 document.addEventListener('click', (e) => {
   const type = e.target.dataset.type;
@@ -26,20 +15,48 @@ document.addEventListener('click', (e) => {
       e.target.tagName.toLowerCase() === 'i' ? e.target : e.target.children[0];
     node.classList.toggle('fa-lock-open');
     node.classList.toggle('fa-lock');
+  } else if (type === 'copy') {
+    copyText(e.target.textContent);
   }
 });
 
-function rgb(red, green, blue) {
-  return (
-    (red & 0xf0 ? '#' : '#0') + ((red << 16) | (green << 8) | blue).toString(16)
-  );
+function createRandomColors() {
+  const colorCode = '0123456789ABCDEF';
+
+  color = '';
+  for (let i = 0; i < 6; i++) {
+    color += colorCode[Math.floor(Math.random() * colorCode.length)];
+  }
+  return '#' + color;
 }
+
+function copyText(text) {
+  return navigator.clipboard.writeText(text);
+}
+// function cracolor() {
+//   const r = Math.random() * 255;
+//   const g = Math.random() * 255;
+//   const b = Math.random() * 255;
+//   return rgb(r, g, b);
+// }
+
+// function rgb(red, green, blue) {
+//   return (
+//     (red & 0xf0 ? '#' : '#0') + ((red << 16) | (green << 8) | blue).toString(16)
+//   );
+// }
 
 function getRandomColor() {
   colons.forEach((col) => {
+    const isLocked = col.querySelector('i').classList.contains('fa-lock');
     const text = col.querySelector('h2');
     const button = col.querySelector('button');
-    const color = cracolor();
+    const color = chroma.random();
+
+    if (isLocked) {
+      return;
+    }
+
     text.textContent = color;
     col.style.background = color;
 
